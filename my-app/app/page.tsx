e52b5ea5f61a268f3353da71222eab11cb2b7e01
Lex <mr.x97@mail.ru>
2026-05-29 14:55:51 +0300

    Checking by removing server-side logic

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,45 +1,50 @@
-import { Table } from 'antd';
-import { calculateTotalDeps, metricsParsed, ToolTableProps } from '../disk_operations';
+//import { Table } from 'antd';
+//import { calculateTotalDeps, metricsParsed, ToolTableProps } from '../disk_operations';
+//
+//const columns = [
+//  {
+//    title: 'RAM',
+//    dataIndex: 'ram',
+//    key: 'ram',
+//  },
+//  {
+//    title: 'CPU',
+//    dataIndex: 'cpu',
+//    key: 'cpu',
+//  }
+//];
+//
+//const SharedResults = () => {
+//	return (
+//		<div></div>
+//	);
+//}
+//
+//const SingleToolTable = ({ title, stats, version, dependencies }: ToolTableProps) => {
+//	console.log({ title });
+//	return (
+//		<div>
+//			<h2>{title.replace(".log", "")} v{version.replace("~", "").replace("^", "")}</h2>
+//			<h3>Dependencies: {calculateTotalDeps(dependencies)}</h3>
+//			<Table columns={columns} dataSource={stats} />
+//		</div>
+//	);
+//}
+//
+//export default function Home() {
+//  return (
+//    <div className="">
+//      <main className="">
+//				{metricsParsed.map(((m, index) =>
+//					<SingleToolTable key={index} {...m}  />
+//				))}
+//				<SharedResults />
+//      </main>
+//    </div>
+//  );
+//}
 
-const columns = [
-  {
-    title: 'RAM',
-    dataIndex: 'ram',
-    key: 'ram',
-  },
-  {
-    title: 'CPU',
-    dataIndex: 'cpu',
-    key: 'cpu',
-  }
-];
-
-const SharedResults = () => {
-	return (
-		<div></div>
-	);
-}
-
-const SingleToolTable = ({ title, stats, version, dependencies }: ToolTableProps) => {
-	console.log({ title });
-	return (
-		<div>
-			<h2>{title.replace(".log", "")} v{version.replace("~", "").replace("^", "")}</h2>
-			<h3>Dependencies: {calculateTotalDeps(dependencies)}</h3>
-			<Table columns={columns} dataSource={stats} />
-		</div>
-	);
-}
-
-export default function Home() {
-  return (
-    <div className="">
-      <main className="">
-				{metricsParsed.map(((m, index) =>
-					<SingleToolTable key={index} {...m}  />
-				))}
-				<SharedResults />
-      </main>
-    </div>
-  );
+export default function Home () {
+	//
+	return <div />
 }
