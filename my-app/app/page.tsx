1526236e2015e2af11f26f7e8a5a3fd0128cc512
Lex <mr.x97@mail.ru>
2026-07-11 19:17:42 +0300

    Added the system information

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,11 +2,27 @@ import { metricsParsed } from '../disk_operations';
 import { SummaryCard } from './summary-card';
 import { SingleToolTable } from './single-tool-table';
 import { ComparisonTable } from './comparison-table';
+import { ram, cores, node_version } from './config.json';
 
 export default function Home() {
   return (
     <main style={{ padding: 32 }}>
       <h1>Comparison of JavaScript Bundlers</h1>
+
+			<div id="environment-banner" className="environment-banner">
+				<div>
+					<span className="label">Node.js</span>
+					<span id="node-version">{node_version}</span>
+				</div>
+				<div>
+					<span className="label">RAM</span>
+					<span id="ram-size">{ram}</span>
+				</div>
+				<div>
+					<span className="label">CPU Cores</span>
+					<span id="cpu-cores">{cores}</span>
+				</div>
+			</div>
 
       <section className="summaryGrid">
         {metricsParsed.map((metric) => (
